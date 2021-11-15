5173e971a6a1c2656d64695b7e90ec3824c1df16
Matheus Melo <matheus.melo.a@hotmail.com>
2021-11-15 01:03:55 -0300

    feat: :art: adicionando o dotenv php para setar váriaveis de conexão e debug

--- apps/busca-dados-consumidores/entities/GerenciadorDeEntidades.php
+++ apps/busca-dados-consumidores/entities/GerenciadorDeEntidades.php
@@ -2,6 +2,7 @@
 
 namespace App\Entities;
 
+use Dotenv\Dotenv;
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 
@@ -10,6 +11,10 @@ class GerenciadorDeEntidades
     protected $entityManager;
     public function __construct()
     {
+        //Carrega as váriaveis do .env
+        $dotenv = Dotenv::createImmutable(__DIR__."/../");
+        $dotenv->load();
+
         // Create a simple "default" Doctrine ORM configuration for Annotations
         $isDevMode = true;
         $proxyDir = null;
@@ -17,22 +22,26 @@ class GerenciadorDeEntidades
         $useSimpleAnnotationReader = false;
         $config = Setup::createAnnotationMetadataConfiguration(array(__DIR__ . "/"), $isDevMode, $proxyDir, $cache, $useSimpleAnnotationReader);
 
-        // replace with mechanism to retrieve EntityManager in your app
-        // database configuration parameters
-        $conn = array(
-            'driver' => 'pdo_pgsql',
-            'host' => 'postgres',
-            'dbname' => 'base_a',
-            'user' => 'postgres',
-            'password' => 'teste', // change to your password
+        $parametrosConexao = array(
+            'driver' => self::env('DB_DRIVER','pdo_pgsql'),
+            'host' => self::env('DB_HOST','127.0.0.1'),
+            'port' => self::env('DB_PORT', 5432),
+            'dbname' => self::env('DB_NAME','postgres'),
+            'user' => self::env('DB_USER','postgres'),
+            'password' => self::env('DB_PASS','')
         );
 
         // obtaining the entity manager
-        $this->entityManager = EntityManager::create($conn, $config);
+        $this->entityManager = EntityManager::create($parametrosConexao, $config);
     }
 
     public function getEntityManager(): EntityManager
     {
         return $this->entityManager;
+    }
+
+    private static function env(string $chave, $valorPadrao)
+    {
+        return !empty($_ENV[$chave]) ? $_ENV[$chave] : $valorPadrao;
     }
 }
--- apps/busca-dados-consumidores/index.php
+++ apps/busca-dados-consumidores/index.php
@@ -2,21 +2,28 @@
 
 require_once "./vendor/autoload.php";
 
+use Dotenv\Dotenv;
 use GraphQL\Type\Schema;
+use GraphQL\Error\DebugFlag;
 use App\Types\ConsumidorQueryType;
 use GraphQL\Server\StandardServer;
-use GraphQL\Error\DebugFlag;
 
 try {
+
+    //Carrega as váriaveis do .env
+    $dotenv = Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+    
     $server = new StandardServer([
         'schema' => new Schema([
             'query' => new ConsumidorQueryType(),
         ]),
         'context' => ["request" => $_REQUEST],
-        'debugFlag' => DebugFlag::INCLUDE_TRACE | DebugFlag::INCLUDE_DEBUG_MESSAGE
+        'debugFlag' => !empty($_ENV['DEBUG_FLAG']) ? (int) $_ENV['DEBUG_FLAG'] : DebugFlag::INCLUDE_DEBUG_MESSAGE,
     ]);
 
     $server->handleRequest();
 } catch (Throwable $error) {
+    echo '<pre>'; print_r($error); echo '</pre>';
     StandardServer::send500Error($error);
 }
